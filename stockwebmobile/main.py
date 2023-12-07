42b4d625785bda46d4f8a73a511916b0c3c276e4
Nangzu <126366721+Nangzu@users.noreply.github.com>
2023-12-07 15:31:44 +0900

    Delete stockwebmobile directory

--- stockwebmobile/main.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, request, redirect, Blueprint
-
-bp = Blueprint('bpmain', __name__, url_prefix='/main')
-@bp.route('/', methods=['POST', 'GET'])
-def main():
-    return render_template('main.html')
